6300def7b3c742ab5d91a49c0bf9ed2a6149e0ee
k-kawaa <kaisei.kawashima@gmail.com>
2024-05-17 22:40:30 +0900

    add GenIPv4Header() and implement version and ihl calc

--- IP/ipv4.c
+++ IP/ipv4.c
@@ -3,4 +3,15 @@
 
 #include "ipv4.h"
 
-uint8_t *GenIPv4Header(ipv4_hdr *hdr);
+
+//no option!!
+uint8_t *GenIPv4Header(ipv4_hdr *hdr){
+    uint8_t *ptr;
+    ptr = malloc(IP_HDR_WITH_NO_OPTION);
+    if (ptr == NULL){
+        printf("[Memory]can't allocate memory");
+        return NULL;
+    }
+    ptr[0] = (hdr->version & 0xFF) << 0 | hdr->ihl << 5;
+    ptr[1] = (hdr->ihl & 0xFF) << 0;
+}
--- IP/ipv4.h
+++ IP/ipv4.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define IP_HDR_WITH_NO_OPTION 20
+
 typedef struct{
     uint8_t version;
     uint8_t ihl;
@@ -14,6 +16,8 @@ typedef struct{
     uint8_t ttl;
     uint8_t protocol;
     uint16_t checksum;
+    uint32_t src;
+    uint32_t dst;
 }ipv4_hdr;
 
 
