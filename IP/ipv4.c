4e579cb60643b6a5f68c9334a2e2edcd4be8a958
k-kawaa <kaisei.kawashima@gmail.com>
2024-05-25 23:42:26 +0900

    Develop now:   IP/ipv4.c

--- IP/ipv4.c
+++ IP/ipv4.c
@@ -3,6 +3,26 @@
 
 #include "ipv4.h"
 
+/*
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |Version|  IHL  |Type of Service|          Total Length         |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |         Identification        |Flags|      Fragment Offset    |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |  Time to Live |    Protocol   |         Header Checksum       |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                       Source Address                          |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Destination Address                        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                    Options                    |    Padding    |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+1 octet is 8 bit!
+*/
 
 //no option!!
 uint8_t *GenIPv4Header(ipv4_hdr *hdr){
@@ -13,5 +33,7 @@ uint8_t *GenIPv4Header(ipv4_hdr *hdr){
         return NULL;
     }
     ptr[0] = (hdr->version & 0xFF) << 0 | hdr->ihl << 5;
-    ptr[1] = (hdr->ihl & 0xFF) << 0;
+    ptr[1] = hdr->tos;
+    ptr[2] = hdr->total_length;
+    
 }
