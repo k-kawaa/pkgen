8ca9c89ac05208810f87aaaf4a61ee0aed687ada
k-kawaa <kaisei.kawashima@gmail.com>
2024-06-22 22:00:36 +0900

    implement IPv4 Header generation code in ipv4.c

--- IP/ipv4.c
+++ IP/ipv4.c
@@ -34,6 +34,23 @@ uint8_t *GenIPv4Header(ipv4_hdr *hdr){
     }
     ptr[0] = (hdr->version & 0xFF) << 0 | hdr->ihl << 5;
     ptr[1] = hdr->tos;
-    ptr[2] = hdr->total_length;
-    
+    ptr[2] = hdr->total_length >> 8;
+    ptr[3] = hdr->total_length & 0x00F;
+    ptr[4] = hdr->idf >> 8;
+    ptr[5] = hdr->idf & 0x0F;
+    ptr[6] = hdr->flags & 0x07 | hdr->fo >> 5;
+    ptr[7] = hdr->flags & 0xFF;
+    ptr[8] = hdr->ttl;
+    ptr[9] = hdr->protocol;
+    ptr[10] = 0;
+    ptr[11] = 0;
+    ptr[12] = (hdr->src >> 24) & 0xFF;
+    ptr[13] = (hdr->src >> 16) & 0xFF;
+    ptr[14] = (hdr->src >> 8)  & 0xFF;
+    ptr[15] = hdr->src & 0xFF;
+    ptr[16] = (hdr->dst >> 24) & 0xFF;
+    ptr[17] = (hdr->dst >> 16) & 0xFF;
+    ptr[18] = (hdr->dst >> 8)  & 0xFF;
+    ptr[19] = hdr->dst & 0xFF;
+
 }
